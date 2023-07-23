b885b25758fb8aa3b629b96b73f98dd4bd690166
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-07-23 20:25:53 +0100

    completed the gallery section

--- src/pages/gallery-page/Gallery.jsx
+++ src/pages/gallery-page/Gallery.jsx
@@ -1,11 +1,30 @@
+/* eslint-disable quotes */
 import React from 'react';
 import { Banner } from '../../components/component-exports';
+import { Image } from '@chakra-ui/image';
+import { sampleImages } from '../../mocked-data/mocked-data';
+
+import './gallery-page.scss'
 
 const Gallery = () => {
     return (
         <>
             <Banner title='Gallery' />
-            {/* <GalleryContainer /> */}
+            <section className='gallery_grid-container'>
+                <section className='gallery_grid-wrapper'>
+                    {sampleImages.map((item, i) => (
+                        <article key={`grid-${i + 1}`}>
+                            <Image
+                                src={item}
+                                objectFit='cover'
+                                height='100%'
+                                width='100%'
+                                alt={`gallery-card-${i}`}
+                            />
+                        </article>
+                    ))}
+                </section>
+            </section>
         </>
     );
 };
