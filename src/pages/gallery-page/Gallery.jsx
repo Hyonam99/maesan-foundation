751270d2d71180bea9ef5b5497d5cd4ff329d154
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-07-25 00:45:24 +0100

    implemented the about page

--- src/pages/gallery-page/Gallery.jsx
+++ src/pages/gallery-page/Gallery.jsx
@@ -5,8 +5,11 @@ import { Image } from '@chakra-ui/image';
 import { sampleImages } from '../../mocked-data/mocked-data';
 
 import './gallery-page.scss'
+import { Box, Modal, ModalBody, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import { RxCross2 } from 'react-icons/rx';
 
 const Gallery = () => {
+    const { isOpen, onClose } = useDisclosure()
     return (
         <>
             <Banner title='Gallery' />
@@ -20,11 +23,30 @@ const Gallery = () => {
                                 height='100%'
                                 width='100%'
                                 alt={`gallery-card-${i}`}
+                                cursor='pointer'
+                                // onClick={onOpen}
                             />
                         </article>
                     ))}
                 </section>
             </section>
+
+            <Modal isOpen={isOpen} onClose={onClose} size={{ base: 'sm', sm: 'md', md: '2xl', lg: '4xl' }} isCentered>
+                <ModalOverlay
+                    bg='none'
+                    backdropFilter='auto'
+                    backdropInvert='10%'
+                    backdropBlur='3px'
+                />
+                <ModalContent>
+                    <Box display='flex' alignItems='center' justifyContent='flex-end' padding='.8rem'>
+                        <RxCross2 onClick={onClose} cursor='pointer' size={26}/>
+                    </Box>
+                    <ModalBody>
+                        <p>some text</p>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </>
     );
 };
