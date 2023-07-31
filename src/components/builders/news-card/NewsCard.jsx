166cccb33c4821fe387d35239f4d89177b80d314
Bassey Hyonam Victor <88059700+Hyonam99@users.noreply.github.com>
2023-07-31 20:13:22 +0100

    integrated the endpoint for getting blogs (#18)

--- src/components/builders/news-card/NewsCard.jsx
+++ src/components/builders/news-card/NewsCard.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 
 import './news-card.scss'
 
-const NewsCard = ({ imgSrc, title, text, sourceId }) => {
+const NewsCard = ({ imgSrc, title, theme, sourceId }) => {
     return (
         <Box className='news-card'>
             <Image src={imgSrc} height={{ base: '132px', sm: '120px' }} width='100%' objectFit='cover' rounded='lg' />
             <Box className='news-card_content'>
                 <Box className='news-card_content_avatar'>
-                    <Avatar size='xs' src={imgSrc}/> <Heading fontSize={{ base: '16px', sm: '18px' }}>{title}</Heading><small>12, May 2023</small>
+                    <Avatar size='xs' src={imgSrc}/> <Heading fontSize={{ base: '16px', sm: '18px' }}>{theme}</Heading><small>12, May 2023</small>
                 </Box>
-                <Text height={{ base: 'initial', sm: '4%', md: '64%' }} fontSize={{ base: '16px', sm: '16px', md: '16px' }} lineHeight={{ base: '5', sm: '8', md: 'initial' }}>{text.substring(0, 44)}...</Text>
+                <Text height={{ base: 'initial', sm: '4%', md: '64%' }} fontSize={{ base: '16px', sm: '16px', md: '16px' }} lineHeight={{ base: '5', sm: '8', md: 'initial' }}>{title?.substring(0, 44)}...</Text>
                 <Box className='link-box'><Link to={`/blog/${sourceId}`}>Read more</Link></Box>
             </Box>
         </Box>
--- src/pages/landing-page/Home.jsx
+++ src/pages/landing-page/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Hero, Featured, Welcome, Mission, Strategy, NewsContainer } from '../../components/component-exports';
-import { newsContent } from '../../mocked-data/mocked-data';
 
 const Home = () => {
     return (
@@ -10,7 +9,7 @@ const Home = () => {
             <Welcome />
             <Mission />
             <Strategy />
-            <NewsContainer mini={3} dataSource={newsContent}/>
+            <NewsContainer mini={3}/>
         </section>
     )
 };
